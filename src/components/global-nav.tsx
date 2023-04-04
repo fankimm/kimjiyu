b98f8f04945c51b0e825c97d864e2ebe071cc681
Jihwan Kim <jihwan.kim@purple.io>
2023-04-05 00:36:03 +0900

    로고 white-space 적용

--- src/components/global-nav.tsx
+++ src/components/global-nav.tsx
@@ -7,7 +7,7 @@ const GlobalNav = () => {
   return (
     <nav className={styles.globalNav}>
       <Link href="/" className={styles.logo}>
-        <div>KIM JIYU</div>
+        <div className={styles.logoName}>KIM JIYU</div>
         <div className={styles.logoDescription}>MAN {diff} YEARS OLD</div>
       </Link>
       <div className={styles.menu}>
