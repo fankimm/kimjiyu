086c208340d7ca55923dba060b61463bacee9d5f
Jihwan Kim <jihwan.kim@purple.io>
2023-04-04 23:16:35 +0900

    만나이 계산 및 표기

--- src/components/global-nav.tsx
+++ src/components/global-nav.tsx
@@ -1,11 +1,14 @@
 import styles from "@/styles/GlobalNav.module.css";
 import Link from "next/link";
 const GlobalNav = () => {
+  const birth = new Date("2018-02-08");
+  const today = new Date()
+  const diff = today.getFullYear() - birth.getFullYear()
   return (
     <nav className={styles.globalNav}>
       <Link href="/" className={styles.logo}>
         <div>KIM JIYU</div>
-        <div className={styles.logoDescription}>6YEARS OLD</div>
+        <div className={styles.logoDescription}>MAN {diff} YEARS OLD</div>
       </Link>
       <div className={styles.menu}>
         <Link href="/">ABOUT</Link>
