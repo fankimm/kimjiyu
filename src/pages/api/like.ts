33e48f3b301e98f3ddeaaacc1430156359e097c3
Jihwan Kim <jihwan.kim@purple.io>
2023-04-07 00:54:34 +0900

    fix 500err

--- src/pages/api/like.ts
+++ src/pages/api/like.ts
@@ -36,22 +36,23 @@ export default async function handler(
       message: "조회성공",
       data: dbRes.data ? dbRes.data : [],
     });
-  }
-  const { id, userId } = JSON.parse(req.body);
-  if (!id || !userId) {
-    res.status(400).json({ status: 400, message: "Bad Request" });
-  }
-  if (method === "POST") {
-    const key = id + userId;
-    const dbRes = await supabase
-      .from("liked")
-      .upsert({ key, userId, parentId: id });
-    res.status(200).json({ status: 200, message: "생성성공" });
-  }
-  if (method === "DELETE") {
-    const key = id + userId;
-    const dbRes = await supabase.from("liked").delete().eq("key", key);
-    // .upsert({ key, userId: body.userId, parentId: id });
-    res.status(200).json({ status: 200, message: "삭제성공" });
+  } else {
+    const { id, userId } = JSON.parse(req.body);
+    if (!id || !userId) {
+      res.status(400).json({ status: 400, message: "Bad Request" });
+    }
+    if (method === "POST") {
+      const key = id + userId;
+      const dbRes = await supabase
+        .from("liked")
+        .upsert({ key, userId, parentId: id });
+      res.status(200).json({ status: 200, message: "생성성공" });
+    }
+    if (method === "DELETE") {
+      const key = id + userId;
+      const dbRes = await supabase.from("liked").delete().eq("key", key);
+      // .upsert({ key, userId: body.userId, parentId: id });
+      res.status(200).json({ status: 200, message: "삭제성공" });
+    }
   }
 }
