c335222aeebcbb54d2f2791439c5b06540208e38
Jihwan Kim <crakim7@naver.com>
2024-06-21 11:14:40 +0900

    codenbutter insert code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,14 @@ export default function App({ Component, pageProps }: AppProps) {
     if (!userId) {
       localStorage.setItem("userId", uuid());
     }
+    (function (co, de, n, but, t, e, r) {
+      !n[co] && (n[co] = function () {
+        (n[co].q = n[co].q || []).push(arguments);
+      }); e = t.createElement(but);
+      e.async = true; e.src = de; r = t.getElementsByTagName(but)[0];
+      r.parentNode.insertBefore(e, r);
+    })("CodenButter", "https://buttr.dev/butter.js", window, "script", document);
+    window.CodenButter("boot", { siteId: "derawretzo", auto: true });
   }, []);
   return (
     <>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,12 +13,6 @@ export default function Document() {
           property="og:image"
           content="https://www.kimjiyu.com/_next/image?url=%2Fgallery%2F2.jpg&w=3840&q=75"
         />
-        <script>(function (co,de,n,but,t,e,r){!n[co]&&(n[co]=function(){
-(n[co].q=n[co].q||[]).push(arguments);});e=t.createElement(but);
-e.async=true;e.src=de;r=t.getElementsByTagName(but)[0];
-r.parentNode.insertBefore(e, r);
-})("CodenButter", "https://buttr.dev/butter.js", window, "script", document);
-window.CodenButter("boot", { siteId: "derawretzo", auto: true });</script>
       </Head>
       <body>
         <Main />
